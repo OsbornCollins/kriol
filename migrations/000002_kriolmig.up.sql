b059570a4c8065e1d4707e194909c69c0c0417cb
OsbornCollins <2012111357@ub.edu.bz>
2022-09-08 23:28:15 -0600

    Added primary key SQL to migration up file.

--- migrations/000002_kriolmig.up.sql
+++ migrations/000002_kriolmig.up.sql
@@ -1,4 +1,5 @@
 CREATE TABLE tblkriol (
-   englishword VARCHAR (50) UNIQUE NOT NULL,
+    id INTEGER PRIMARY KEY,
+    englishword VARCHAR (50) UNIQUE NOT NULL,
     kriolword VARCHAR (50) UNIQUE NOT NULL 
 )
